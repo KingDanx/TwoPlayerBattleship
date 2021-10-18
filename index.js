2c3ac9bb059ee198cded6ee77289978c8d6f8ada
Dan Prudhomme <dannyprud@yahoo.com>
2021-10-17 20:10:15 -0500

    create player one and two war ships.  create exports from player1 and player2 and imported to index.js. create function to print a board readout in index.js

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 "use strict";
 const prompt = require("prompt-sync")();
+let {playerOneBoard, playerOneDestroyer, playerOneSub, playerOneBattleship1, playerOneBattleship2, playerOneCarrier} = require("./player1");
+let {playerTwoBoard, playerTwoDestroyer, playerTwoSub, playerTwoBattleship1, playerTwoBattleship2, playerTwoCarrier} = require("./player2");
+
 console.log("testing");
+console.log(playerTwoBattleship1);
+
+//for displaying fullboard
+function printPlayersBoard(playerBoardArray){
+    for(let i = 0; i < playerBoardArray.length; i++){
+        console.log(playerBoardArray[i].toString());
+    }
+}
+
+printPlayersBoard(playerOneBoard);
+
+
+
