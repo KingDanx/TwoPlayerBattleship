8ee4af8f199fe8b4e53045ed23815baca89d1d68
Dan Prudhomme <dannyprud@yahoo.com>
2021-10-18 14:25:12 -0500

    add working function to get user input as a letter and return a number that can be used as an array index for the board cooridinates. create skeleton for function to place ships at start of game

--- index.js
+++ index.js
@@ -13,7 +13,65 @@ function printPlayersBoard(playerBoardArray){
     }
 }
 
-printPlayersBoard(playerTwoBoard);
+console.log("Enter letter to place your Destoryer (2 blocks)")
 
+function placeShip(row, col){
+   
+}
+
+function rowSelect(){
+    console.log("Enter the letter of the row.");
+    let userInput = prompt();
+    userInput = userInput.trim().toUpperCase();
+    switch (userInput) {
+        case "A":
+            return 1;
+        case "B":
+            return 2;
+        case "C":
+            return 3;
+        case "D":
+            return 4;
+        case "E":
+            return 5;
+        case "F":
+            return 6;
+        case "G":
+            return 7;
+        case "H":
+            return 8;
+        case "I":
+            return 9;
+        case "J":
+            return 10;
+        case "K":
+            return 11;
+        case "L":
+            return 12;
+        case "M":
+            return 13;
+        case "N":
+            return 14;
+        case "O":
+            return 15;
+        case "P":
+            return 16;
+        case "Q":
+            return 17;
+        case "R":
+            return 18;
+        case "S":
+            return 19
+        case "T":
+            return 20;
+        default:
+            console.log("Invalid input. Try again.");
+            rowSelect();
+            break;
+    }
+}
+
+printPlayersBoard(playerTwoBoard);
+rowSelect();
 
 
